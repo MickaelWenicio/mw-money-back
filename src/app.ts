942fe71bc87af41bf2d8258fea5eea483c4f6d6d
Mickael Wenicio <mickael.programador123@gmail.com>
2025-05-19 06:24:01 -0300

    feat: initial routes configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import router from './routes/routes';
 
 const app = express();
 dotenv.config();
@@ -8,9 +9,6 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api", router);
 
 export default app;
